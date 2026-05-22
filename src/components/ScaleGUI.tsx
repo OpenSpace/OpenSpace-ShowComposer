65ac9c1721e47a89d77de528d0fbc042f545a6b5
Andreas Engberg <adde.engberg@gmail.com>
2026-05-22 12:13:33 +0200

    lint

--- src/components/ScaleGUI.tsx
+++ src/components/ScaleGUI.tsx
@@ -30,7 +30,11 @@ const ScaleGUI: React.FC = () => {
         max={200}
         min={75}
       />
-      <div className={"pointer-events-none absolute inset-y-0 left-0 ml-10 flex  items-center"}>
+      <div
+        className={
+          'pointer-events-none absolute inset-y-0 left-0 ml-10 flex items-center'
+        }
+      >
         <span className={'text-sm'}>{getCopy('ScaleGUI', '%')}</span>
       </div>
       <HoldButton variant={'outline'} size={'icon'} onClick={zoomOut}>
