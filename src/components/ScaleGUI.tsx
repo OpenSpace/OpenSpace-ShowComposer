b52a1defd1df460cdfb75c2c4a10a35396ff3684
Ylva Selling <ylva.selling@gmail.com>
2026-07-06 15:41:29 +0200

    Migrate holdbutton to mantine

--- src/components/ScaleGUI.tsx
+++ src/components/ScaleGUI.tsx
@@ -17,8 +17,8 @@ const ScaleGUI: React.FC = () => {
   };
   return (
     <div className={'flex w-16 flex-col items-center justify-center gap-2'}>
-      <HoldButton variant={'outline'} size={'icon'} onClick={zoomIn}>
-        <ZoomIn size={'16'} />
+      <HoldButton variant={'filled'} onClick={zoomIn}>
+        <ZoomIn size={'16'} color={'white'} />
       </HoldButton>
       <Input
         value={Math.round(scale * 100)}
@@ -37,8 +37,8 @@ const ScaleGUI: React.FC = () => {
       >
         <span className={'text-sm'}>{getCopy('ScaleGUI', '%')}</span>
       </div>
-      <HoldButton variant={'outline'} size={'icon'} onClick={zoomOut}>
-        <ZoomOut size={'16'} />
+      <HoldButton variant={'filled'} onClick={zoomOut}>
+        <ZoomOut size={'16'} color={'white'} />
       </HoldButton>
     </div>
   );
--- src/components/common/HoldButton.tsx
+++ src/components/common/HoldButton.tsx
@@ -1,19 +1,12 @@
-import { useRef } from 'react';
-import { VariantProps } from 'class-variance-authority';
+import React, { useRef } from 'react';
+import { ActionIcon, ActionIconProps } from '@mantine/core';
 
-import { Button, buttonVariants } from '../ui/button';
-
-interface HoldButtonProps extends VariantProps<typeof buttonVariants> {
+interface HoldButtonProps extends ActionIconProps {
   onClick: () => void;
   children: React.ReactNode;
 }
 
-const HoldButton: React.FC<HoldButtonProps> = ({
-  variant,
-  size,
-  onClick,
-  children
-}: HoldButtonProps) => {
+const HoldButton: React.FC<HoldButtonProps> = ({ onClick, children, ...props }) => {
   const clickInterval = useRef<NodeJS.Timeout | null>(null);
   const handleMouseDown = () => {
     onClick();
@@ -27,16 +20,15 @@ const HoldButton: React.FC<HoldButtonProps> = ({
   };
 
   return (
-    <Button
-      variant={variant}
-      size={size}
+    <ActionIcon
+      {...props}
       onMouseDown={handleMouseDown}
       onMouseUp={handleMouseUp}
       onMouseLeave={handleMouseUp}
       onClick={onClick}
     >
       {children}
-    </Button>
+    </ActionIcon>
   );
 };
 
