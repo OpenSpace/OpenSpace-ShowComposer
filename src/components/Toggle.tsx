3fc66c4aa26d94fdc9ec403edd5332509628901a
Ylva Selling <ylva.selling@gmail.com>
2026-07-15 14:24:00 +0200

    Add left position prop to toggle

--- src/components/Toggle.tsx
+++ src/components/Toggle.tsx
@@ -5,6 +5,7 @@ interface ToggleComponentProps {
   value: boolean;
   setValue: (value: boolean) => void;
   label?: string;
+  labelPosition?: 'left' | 'right';
   disabled?: boolean;
   className?: string;
 }
@@ -14,12 +15,14 @@ const Toggle: React.FC<ToggleComponentProps> = ({
   setValue,
   disabled = false,
   label,
+  labelPosition,
   className = ''
 }) => {
   return (
     <Checkbox
       className={className}
       label={label}
+      labelPosition={labelPosition}
       disabled={disabled}
       checked={value}
       onChange={(event) => setValue(event.currentTarget.checked)}
