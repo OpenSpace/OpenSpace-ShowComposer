a1d11ac17103c7a60a8dbc7543d3e0ebafb04022
Ylva Selling <ylva.selling@gmail.com>
2026-07-23 15:08:14 +0200

    Migrate Undo to mantine

--- src/editor/sidebar/Undo.tsx
+++ src/editor/sidebar/Undo.tsx
@@ -1,38 +1,36 @@
-import { ActionIcon, Button } from '@mantine/core';
-import { Redo as RedoIcon, Undo as UndoIcon } from 'lucide-react';
+import { ActionIcon, Button, Group, Text } from '@mantine/core';
 
+import { RedoIcon, UndoIcon } from '@/icons/icons';
 import { useBoundStoreTemporal } from '@/store/boundStore';
-const Undo = () => {
+
+export default function Undo() {
   const { undo, redo, clear, pastStates, futureStates } = useBoundStoreTemporal(
     (state) => state
   );
   return (
-    <div>
-      <div className={'flex w-full flex-row items-center justify-start gap-2'}>
-        <h2 className={' text-xs font-bold'}>History:</h2>
-        <ActionIcon
-          variant={'filled'}
-          onClick={() => undo()}
-          disabled={!pastStates.length}
-          size={'lg'}
-        >
-          <UndoIcon size={16} />
-        </ActionIcon>
-        <ActionIcon
-          variant={'filled'}
-          onClick={() => redo()}
-          disabled={!futureStates.length}
-          size={'lg'}
-        >
-          <RedoIcon size={16} />
-        </ActionIcon>
-        <Button size={'sm'} variant={'subtle'} onClick={() => clear()}>
-          Clear
-        </Button>
-      </div>
-      {/* {currentCommand?.description} */}
-      {/* Rest of your editor */}
-    </div>
+    <Group w={'100%'} gap={'xs'}>
+      <Text size={'xs'} fw={700}>
+        History:
+      </Text>
+      <ActionIcon
+        variant={'filled'}
+        onClick={() => undo()}
+        disabled={!pastStates.length}
+        size={'lg'}
+      >
+        <UndoIcon size={16} />
+      </ActionIcon>
+      <ActionIcon
+        variant={'filled'}
+        onClick={() => redo()}
+        disabled={!futureStates.length}
+        size={'lg'}
+      >
+        <RedoIcon size={16} />
+      </ActionIcon>
+      <Button size={'sm'} variant={'subtle'} onClick={() => clear()}>
+        Clear
+      </Button>
+    </Group>
   );
-};
-export default Undo;
+}
