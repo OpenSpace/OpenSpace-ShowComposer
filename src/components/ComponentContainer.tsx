d88a265c90a5d666ae54926da4a346640fb8e418
Ylva Selling <ylva.selling@gmail.com>
2026-07-29 09:29:58 +0200

    Migrate component container to mantine

--- src/components/ComponentContainer.tsx
+++ src/components/ComponentContainer.tsx
@@ -1,41 +1,44 @@
-import clsx from 'clsx';
+import { Flex } from '@mantine/core';
 
 interface ComponentContainerProps {
   children: React.ReactNode;
   onClick?: () => void;
   backgroundImage?: string;
   backgroundColor?: string;
-  className?: string;
   style?: React.CSSProperties;
 }
 
-const ComponentContainer: React.FC<ComponentContainerProps> = ({
+function ComponentContainer({
   children,
   onClick,
   backgroundImage,
   backgroundColor,
-  className,
   style
-}) => {
+}: ComponentContainerProps) {
   return (
-    <div
-      className={clsx(
-        'absolute right-0 top-0 flex h-full w-full cursor-pointer items-center justify-center rounded-md',
-        className
-      )}
+    <Flex
+      pos={'absolute'}
+      top={0}
+      right={0}
+      h={'100%'}
+      w={'100%'}
+      align={'center'}
+      justify={'center'}
+      onClick={onClick}
       style={{
-        //cover and center the background image
+        cursor: 'pointer',
+        borderRadius: 'var(--mantine-radius-md)',
+        // cover and center the background image
         backgroundSize: 'cover',
         backgroundPosition: 'center',
         backgroundImage: `url(${backgroundImage})`,
         backgroundColor: backgroundColor,
         ...style
       }}
-      onClick={onClick}
     >
       {children}
-    </div>
+    </Flex>
   );
-};
+}
 
 export default ComponentContainer;
