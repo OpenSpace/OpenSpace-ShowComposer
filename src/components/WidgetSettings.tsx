b451c01c3a6c655cfc184dafdf982823fec72ce8
Ylva Selling <ylva.selling@gmail.com>
2026-08-05 11:42:00 +0200

    Move background picker into widget settings

--- src/components/WidgetSettings.tsx
+++ src/components/WidgetSettings.tsx
@@ -1,6 +1,8 @@
-import { Group, Textarea, TextInput } from '@mantine/core';
+import { Group, SimpleGrid, Stack, Text, Textarea, TextInput } from '@mantine/core';
 
-import { BackgroundPicker } from '@/components/BackgroundPicker';
+import { ColorPicker } from '@/components/ColorPicker';
+import { Image } from '@/components/Image';
+import { ImageUpload } from '@/components/ImageUpload';
 import { Toggle as ToggleComponent } from '@/components/Toggle';
 import { getCopy } from '@/utils/copyHelpers';
 
@@ -52,12 +54,29 @@ function WidgetSettings({
       ) : (
         nameInput
       )}
-      <BackgroundPicker
-        color={color}
-        setColor={setColor}
-        backgroundImage={backgroundImage}
-        setBackgroundImage={setBackgroundImage}
-      />
+      <Stack gap={'md'}>
+        <SimpleGrid cols={2} spacing={'md'}>
+          <Stack gap={'md'}>
+            <Text size={'sm'} fw={500}>
+              {getCopy('WidgetSettings', 'background_color')}
+            </Text>
+            <ColorPicker color={color} setColor={setColor} />
+          </Stack>
+          <Stack gap={'md'}>
+            <Text size={'sm'} fw={500}>
+              {getCopy('WidgetSettings', 'background_image')}
+            </Text>
+            <Image
+              w={backgroundImage.length > 0 ? 128 : 64}
+              h={backgroundImage.length > 0 ? 128 : 64}
+              fit={'cover'}
+              src={backgroundImage || ''}
+              alt={'Loaded'}
+            />
+          </Stack>
+        </SimpleGrid>
+        <ImageUpload value={backgroundImage} onChange={setBackgroundImage} />
+      </Stack>
       <Textarea
         id={'description'}
         label={getCopy('WidgetSettings', 'gui_description')}
